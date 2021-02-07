26dacd9912b35f606e5118765940a1a42ac3224a
josdion <josdion@users.noreply.github.com>
2021-02-07 11:00:09 +0200

    WIP

---
+++ AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("subbuzz")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © josdion, 2021")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Subtitles plugin for emby and jellyfin")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.1")]
+[assembly: System.Reflection.AssemblyProductAttribute("subbuzz")]
+[assembly: System.Reflection.AssemblyTitleAttribute("subbuzz")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.1")]
+
