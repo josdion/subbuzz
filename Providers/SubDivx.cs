98c3985f5da92bb8851d4d7bf3bada29e67354ab
josdion <josdion@users.noreply.github.com>
2023-08-30 13:55:03 +0300

    Initail implementation of subdivx provider

---
+++ Providers/SubDivx.cs
@@ -0,0 +1,70 @@
+﻿using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Controller.Subtitles;
+using MediaBrowser.Model.Globalization;
+using MediaBrowser.Model.IO;
+using MediaBrowser.Model.Providers;
+using subbuzz.Configuration;
+using subbuzz.Helpers;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace subbuzz.Providers
+{
+    public class SubDivx : ISubBuzzProvider
+    {
+        internal const string NAME = "subdivx";
+        private const string ServerUrl = "https://subf2m.co";
+        private static readonly string[] CacheRegionSub = { "subdivx", "sub" };
+        private static readonly string[] CacheRegionSubPage = { "subdivx", "subpage" };
+        private static readonly string[] CacheRegionSearch = { "subdivx", "search" };
+
+        private readonly Logger _logger;
+        private readonly Http.Download _downloader;
+        private readonly IFileSystem _fileSystem;
+        private readonly ILocalizationManager _localizationManager;
+        private readonly ILibraryManager _libraryManager;
+
+        public string Name => $"[{Plugin.NAME}] <b>{NAME}</b>";
+
+        public IEnumerable<VideoContentType> SupportedMediaTypes =>
+            new List<VideoContentType> { VideoContentType.Episode, VideoContentType.Movie };
+
+        private PluginConfiguration GetOptions()
+            => Plugin.Instance.Configuration;
+
+        public SubDivx(
+            Logger logger,
+            IFileSystem fileSystem,
+            ILocalizationManager localizationManager,
+            ILibraryManager libraryManager)
+        {
+            _logger = logger;
+            _fileSystem = fileSystem;
+            _localizationManager = localizationManager;
+            _libraryManager = libraryManager;
+            _downloader = new Http.Download(logger);
+
+        }
+
+        public async Task<SubtitleResponse> GetSubtitles(string id, CancellationToken cancellationToken)
+        {
+            return await _downloader.GetSubtitles(id, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<RemoteSubtitleInfo>> Search(SubtitleSearchRequest request,
+            CancellationToken cancellationToken)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var res = new List<SubtitleInfo>();
+
+            // TODO: 
+
+            watch.Stop();
+            _logger.LogInformation($"Search duration: {watch.ElapsedMilliseconds / 1000.0} sec. Subtitles found: {res.Count}");
+
+            return res;
+        }
+    }
+}
