e4090112c4ce883285a34dcd8863af8d995ae31c
josdion <josdion@users.noreply.github.com>
2025-12-20 15:16:39 +0200

    Prepare support for Jellyfin 10.11

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -4,9 +4,9 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("subbuzz")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © josdion, 2025")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Subtitles plugin for emby and jellyfin")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.2.2.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.2.2.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("subbuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("subbuzz")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.2.2.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.3.0.0")]
 
